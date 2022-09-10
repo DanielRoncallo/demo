636118ec74194120937e0f92f9e1bb7789482a55
DanielRoncallo <roncallodaniel@hotmail.com>
2022-09-10 10:37:25 -0500

    menu and grab ray

--- Assets/ActivateTpRay.cs
+++ Assets/ActivateTpRay.cs
@@ -15,11 +15,20 @@ public class ActivateTpRay : MonoBehaviour
     public InputActionProperty leftCancel;
     public InputActionProperty rightCancel;
 
+    public XRRayInteractor leftRay;
+    public XRRayInteractor rightRay;
 
     // Update is called once per frame
     void Update()
     {
-        leftTelePort.SetActive(leftCancel.action.ReadValue<float>()==0 && leftActivate.action.ReadValue<float>() > 0.1f);
-        rightTelePort.SetActive(rightCancel.action.ReadValue<float>()==0 && RightActivate.action.ReadValue<float>() > 0.1f);
+        bool isLeftRayHovering = leftRay.TryGetHitInfo(out Vector3 leftPos,out Vector3 leftNormal
+        ,out int leftNumber, out bool leftValid);
+
+        leftTelePort.SetActive(!isLeftRayHovering && leftCancel.action.ReadValue<float>()==0 && leftActivate.action.ReadValue<float>() > 0.1f);
+        
+        bool isRightRayHovering = leftRay.TryGetHitInfo(out Vector3 rightPos,out Vector3 rightNormal
+        ,out int rightNumber, out bool rightValid);
+        
+        rightTelePort.SetActive(!isRightRayHovering && rightCancel.action.ReadValue<float>()==0 && RightActivate.action.ReadValue<float>() > 0.1f);
     }
 }
